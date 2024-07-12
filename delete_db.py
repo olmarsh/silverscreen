ca7c95ab9afd18de0b2f8033ec6926692383b7a1
olmarsh <marsh.oliver@hotmail.com>
2024-07-12 15:45:14 +1200

    Add database delete script

---
+++ delete_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect("silverscreen.db")
+print("Connected to database")
+
+delete_id = input('Which movie ID to delete: ')
+
+conn.execute(f'''DELETE FROM Movies
+WHERE ID = {delete_id};
+''');
+
+print('Deleted entry successfully')
+
+conn.commit();
