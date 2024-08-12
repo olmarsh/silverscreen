d5e47522d6d1aeda8a5b0869e8beea442e8d49b2
olmarsh <marsh.oliver@hotmail.com>
2024-08-13 09:52:46 +1200

    Create basic navigation structure

--- src/ss_flask.py
+++ src/ss_flask.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def homepage():
-    return 'silverscreen movie database'
+def index():
+    return render_template('index.html')
+
+@app.route('/movies')
+def movies():
+    return render_template('movies.html')
 
 if __name__ == '__main__':
    app.run()
